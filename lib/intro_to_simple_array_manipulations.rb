dd11975ec17b10cf16587d5ef436f60fbb99c1ec
Ignacio R Espino <irespino@capozios.com>
2018-08-22 22:20:32 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,18 @@
+def using_push(array, string)
+  array.push(string)  
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array, string)
+  array.pop(string)
+end
+
+def pop_with_args(array, arg)
+  popped = []
+  popped.push(array.pop)
+  popped.push(array.pop)
+  popped
+end
